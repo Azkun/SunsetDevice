fe34d9f2aa0dec2cea0555d0fd46d38777bb5e00
Azkun <adam.leghribi@gmail.com>
2023-02-23 15:52:34 +0100

    Adding html pages

--- webserver/app.js
+++ webserver/app.js
@@ -1,18 +1,27 @@
 import express from 'express'
 import chalk from 'chalk'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const app = express();
 const port = 3000;
 
-function getlog(name,user,hostname) {
-    console.log("["+chalk.green("GET Request")+"] - From "+chalk.blueBright(user)+" - To "+chalk.blueBright.underline(hostname+":"+port+name))
+function getlog(name, user, hostname) {
+    console.log("[" + chalk.green("GET Request") + "] - From " + chalk.blueBright(user) + " - To " + chalk.blueBright.underline(hostname + ":" + port + name))
 }
 
-app.get('/', function(req, res) {
-    getlog("/",req.ip,req.hostname)
-    res.send('Hello world!');
+app.get('/', function (req, res) {
+    getlog("/", req.ip, req.hostname)
+    res.type("html")
+    res.sendFile(path.join(__dirname, "/front-end/index.html"))
 });
 
+app.use(express.static(path.join(__dirname, '/front-end/')));
 app.listen(port, () => {
-    console.log("["+chalk.blueBright("Launching")+"] - App listening on port "+chalk.magenta(port))
+    console.log("[" + chalk.blueBright("Launching") + "] - App listening on port " + chalk.magenta(port))
 })
 
