a21be1c8c6e7b30c4cf83861c9942c6789bd7f3d
Azkun <adam.leghribi@gmail.com>
2023-02-23 20:59:51 +0100

    Added a new endpoint "/request"

--- webserver/app.js
+++ webserver/app.js
@@ -10,8 +10,13 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = 3000;
 
-function getlog(name, user, hostname) {
-    console.log("[" + chalk.green("GET Request") + "] - From " + chalk.blueBright(user) + " - To " + chalk.blueBright.underline(hostname + ":" + port + name))
+function getlog(name, user, hostname, additional = "content_query="+undefined) {
+    var add = " - "
+    if (additional == "content_query="+undefined) {
+        add = ""
+        additional = ""
+    }
+    console.log("[" + chalk.green("GET Request") + "] - From " + chalk.blueBright(user) + " - To " + chalk.blueBright.underline(hostname + ":" + port + name) + add + additional)
 }
 
 app.get('/', function (req, res) {
@@ -19,6 +24,13 @@ app.get('/', function (req, res) {
     res.type("html")
     res.sendFile(path.join(__dirname, "/front-end/index.html"))
 });
+
+app.get('/request', function (req, res) {
+    getlog("/request", req.ip, req.hostname, "content_query="+req.query.content)
+    res.status(200)
+    res.type("application/json")
+    res.send({content:req.query.content,status:200})
+})
 
 app.use(express.static(path.join(__dirname, '/front-end/')));
 app.listen(port, () => {
